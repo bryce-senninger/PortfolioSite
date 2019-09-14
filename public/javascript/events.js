8f64ecca943877c806fd5248b529a764bd4fb6c9
Bryce <bryce.senninger@gmail.com>
2019-09-13 21:40:10 -0600

    finished portfolio site update

--- public/javascript/events.js
+++ public/javascript/events.js
@@ -9,3 +9,14 @@ $(".projectJumbo").click(function(){
 $(".contactJumbo").click(function(){
     window.location.href = "/contact"
 });
+
+$(".techJobJumbo").click(function(){
+    window.location.href = ""
+});
+
+$(".readyMetGoJumbo").click(function(){
+    window.location.href = "https://readymetgo.herokuapp.com/"
+});
+$(".recipeJumbo").click(function(){
+    window.location.href = "https://trackrecipes.herokuapp.com"
+});
